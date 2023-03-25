685a68812779a7c591387f550eca97867a7bfe7c
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-03-25 16:05:24 +0900

    메뉴 UI 1차

---
+++ FillUpNow/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  FillUpNow
+//
+//  Created by 한소희 on 2023/03/25.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.selectedIndex = 1
+            
+    }
+}
---
+++ FillUpNow/Menu/SigninViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SigninViewController.swift
+//  FillUpNow
+//
+//  Created by 한소희 on 2023/03/25.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
