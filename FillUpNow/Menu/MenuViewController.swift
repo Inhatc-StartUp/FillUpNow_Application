1040076275dc97b6fae3c1f0c62535e3e9d37537
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-03-29 15:11:03 +0900

    로그인 UI 2차

--- FillUpNow/Menu/MenuViewController.swift
+++ FillUpNow/Menu/MenuViewController.swift
@@ -10,15 +10,10 @@ import UIKit
 class MenuViewController: UIViewController {
     
     @IBOutlet weak var userInfoButton: UIButton!
-    
     @IBOutlet weak var signinButton: UIButton!
-    
     @IBOutlet weak var signupButton: UIButton!
-    
     @IBOutlet weak var noticeButton: UIButton!
-    
     @IBOutlet weak var pushAlarmSwitch: UISwitch!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
