c02cd8496d4d25c9f5f824ee286db9c599767dc9
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-03-29 14:35:04 +0900

    로그인/회원가입 UI 1차

---
+++ FillUpNow/Menu/SigninEmailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SigninEmailViewController.swift
+//  FillUpNow
+//
+//  Created by 한소희 on 2023/03/29.
+//
+
+import UIKit
+
+class SigninEmailViewController: UIViewController {
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func loginActionButton(_ sender: UIButton) {
+    }
+    
+}
