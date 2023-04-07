a19d4665ba50debe2bfd48c1d1ad34506af31920
PyoYurim <pyoqhfk1225@naver.com>
2023-04-07 12:53:24 +0900

    pull request 수정 및 로그인 로직 구현

---
+++ FillUpNow/Menu/NoticeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoticeViewController.swift
+//  FillUpNow
+//
+//  Created by 한소희 on 2023/03/25.
+//
+
+import UIKit
+
+final class NoticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ FillUpNow/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  FillUpNow
+//
+//  Created by 한소희 on 2023/03/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
