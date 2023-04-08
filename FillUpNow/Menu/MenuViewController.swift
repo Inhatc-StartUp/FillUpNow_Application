e21eb6128bed5b795ae93ce0ca3347e4ff633ae5
아기코쟁이 <90755590+haansohee@users.noreply.github.com>
2023-04-08 14:45:13 +0900

    회원정보뷰 포함  기본 UI 구성 3차

--- FillUpNow/Menu/MenuViewController.swift
+++ FillUpNow/Menu/MenuViewController.swift
@@ -11,13 +11,29 @@ final class MenuViewController: UIViewController {
     
     @IBOutlet weak var userInfoButton: UIButton!
     @IBOutlet weak var signinButton: UIButton!
-    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var signoutButton: UIButton!
     @IBOutlet weak var noticeButton: UIButton!
     @IBOutlet weak var pushAlarmSwitch: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        userInfoButton.layer.cornerRadius = 15
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        signoutButton.isHidden = true
+    }
+    
+    @IBAction func pushAlarmSwitchTapped(_ sender: UISwitch) {
+    }
+    
+    @IBAction func signinButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func signoutButtonTapped(_ sender: UIButton) {
+    }
+    
 
 }
